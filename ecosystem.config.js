c4d8bf6624bb2c3def1daddfd0db7b8a5c4dcb32
fkkmemi <fkkmemi@gmail.com>
2018-03-26 17:11:42 +0900

    pm2 ecosystem bug fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -32,30 +32,30 @@ module.exports = {
    */
   deploy : {
     production : {
-      user : 'cfg.pm2.user',
+      user : cfg.pm2.user,
       host : [
           {
-              "host": "cfg.pm2.host",
-              "port": "cfg.pm2.port"
+              "host": cfg.pm2.host,
+              "port": cfg.pm2.port,
           }
         ],
       ref  : 'origin/master',
       repo : 'git@github.com:fkkmemi/nembv.git',
-      path : '/var/www/nembv',
+      path : cfg.pm2.path,
         // 'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
       'post-deploy' : 'npm install && cd fe && npm install && npm run build && cd .. && pm2 startOrRestart ecosystem.config.js --env production'
     },
     dev : {
-      user : 'cfg.pm2.user',
+      user : cfg.pm2.user,
       host : [
         {
-          "host": "cfg.pm2.host",
-          "port": "cfg.pm2.port"
+          "host": cfg.pm2.host,
+          "port": cfg.pm2.port,
         }
       ],
       ref  : 'origin/master',
       repo : 'git@github.com:fkkmemi/nembv.git',
-      path : '/var/www/nembv',
+      path : cfg.pm2.path,
       'post-deploy' : 'npm install && cd fe && npm install && npm run build && cd .. && pm2 startOrRestart ecosystem.config.js --env dev',
       env  : {
         NODE_ENV: 'dev'
